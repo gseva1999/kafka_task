f719d86feb900b46850369668da39eae69691540
Seva <65896047+gseva1999@users.noreply.github.com>
2022-06-24 20:08:53 +0300

    Update producer.py

--- producer.py
+++ producer.py
@@ -10,11 +10,17 @@ producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 consumer = KafkaConsumer(
     topic, bootstrap_servers=bootstrap_servers, auto_offset_reset='earliest')
 
-while True:
-    num = str(random.randint(1, 100))
-    future = producer.send(topic, num.encode('utf-8'), partition=0)
-    print(num)
-    word = ''.join(random.choice(string.ascii_lowercase) for _ in range(8))
-    future2 = producer.send(topic, word.encode('utf-8'), partition=1)
-    print(word)
-    sleep(2)
+
+def main():
+    while True:
+        num = str(random.randint(1, 100))
+        future = producer.send(topic, num.encode('utf-8'), partition=0)
+        print(num)
+        word = ''.join(random.choice(string.ascii_lowercase) for _ in range(8))
+        future2 = producer.send(topic, word.encode('utf-8'), partition=1)
+        print(word)
+        sleep(2)
+ 
+
+if __name__ == "__main__":
+    main()
